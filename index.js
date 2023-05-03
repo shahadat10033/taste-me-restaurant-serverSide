b1bfa76687887b3029e816c4193a4e8aa9a5fa58
shahadat10033 <shahadat10033@gmail.com>
2023-05-03 14:06:07 +0600

    using cors function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const data = require("express");
+const data = require("./data.json");
+const cors = require("cors");
 const app = express();
 const port = 5000;
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 app.get("/data", (req, res) => {
-  res.send("Hello from server");
+  res.send(data);
 });
 
 app.listen(port, () => {
